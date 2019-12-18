956580a4e5e0017dd834246c4d24f97bbd39b5eb
yadavj795 <53616701+yadavj795@users.noreply.github.com>
2019-12-18 13:37:34 +0000

    Spark-kakfa-ingest

---
+++ Spark-kakfa-ingest.py
@@ -0,0 +1,104 @@
+import tweepy,time
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from kafka import SimpleProducer, KafkaClient
+from datetime import datetime, timedelta,date
+from kafka import KafkaConsumer, KafkaProducer
+import sys
+#reload(sys)
+#sys.setdefaultencoding('utf8')
+
+today = str(date.today())
+
+def normalize_timestamp(time):
+    mytime = datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
+    mytime += timedelta(hours=1)   # the tweets are timestamped in GMT timezone, while I am in +1 timezone
+    return (mytime.strftime("%Y-%m-%d %H:%M:%S"))
+    
+consumer_key = "xxxx"
+consumer_secret = "xxxx"
+access_token = "xxxx"
+access_token_secret = "xxxx"
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth, wait_on_rate_limit=True) #wait for rate limit replenish without exit
+
+query = "#hadoop OR #Spark OR #IOT OR #BigData OR #Blockchain"
+max_tweets = 100
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+topic_name = 'tweets'
+
+
+def get_twitter_data():
+    res = api.search(query)
+
+    for i in res:
+        record = ''
+        record += str(normalize_timestamp(str(i.created_at)))
+        record += ','
+        record += str(i.id)
+        record += ','
+        record += str(i.user.id_str)
+        record += ','
+        record += i.text.encode('utf-8').strip()
+        record += ','
+        record += str(i.source)
+        record += ','
+        record += str(i.in_reply_to_status_id)
+        record += ','
+        record += str(i.in_reply_to_status_id_str)
+        record += ','
+        record += str(i.in_reply_to_user_id)
+        record += ','
+        record += str(i.in_reply_to_user_id_str)
+        record += ','
+        record += str(i.in_reply_to_screen_name)
+        record += ','
+        record += str(i.geo)
+        record += ','
+        record += str(i.coordinates)
+        record += ','
+        record += str(i.place)
+        record += ','
+        record += str(i.user.followers_count)
+        record += ','
+        record += str(i.user.friends_count)
+        record += ','
+        record += str(i.user.favourites_count)
+        record += ','
+        record += str(i.user.statuses_count)
+        record += ','
+        record += str(i.user.location)
+        record += ','
+        record += str(i.favorite_count)
+        record += ','
+        record += str(i.retweet_count)
+        record += ','
+        record += str(i.user.screen_name)
+        record += ','
+        record += str(i.user.name)
+        record += ','
+        record += str(i.lang)
+        print(record)
+        #producer.send(topic_name, record)
+
+def get_twitter_data_json():
+    searched_tweets = [status._json for status in tweepy.Cursor(api.search,  q=query).items(max_tweets)]
+    for json_obj in searched_tweets:
+        rddjson = sc.parallelize([json.dumps(json_obj).encode('utf-8')])
+        df = sqlContext.read.option("multiLine", "true").option("mode", "DROPMALFORMED").json(rddjson)
+      
+        producer.send(topic_name, json.dumps(json_obj).encode('utf-8'))
+
+def periodic_work(interval):
+    while True:
+        get_twitter_data_json()
+        #interval should be an integer, the number of seconds to wait
+        time.sleep(interval)
+        
+periodic_work(60 * 0.2) 
+
+
