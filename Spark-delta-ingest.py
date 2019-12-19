1a2247575446ca1e2d8cf48e89e9646846d70bdf
yadavj795 <53616701+yadavj795@users.noreply.github.com>
2019-12-19 11:12:17 +0000

    removed dedup

--- Spark-delta-ingest.py
+++ Spark-delta-ingest.py
@@ -34,12 +34,8 @@ kafkaStreamDF = spark.readStream.format("kafka").option("kafka.bootstrap.servers
 
 json_df = kafkaStreamDF.select(from_json(col("value"),Schema_json).alias("values"))
 
-
 flattenedJsonDf = json_df.select(col("values.created_at").alias("created_at"),col("values.user.screen_name").alias("screen_name"),col("values.user.name").alias("name"),col("values.user.followers_count").alias("followers_count"),col("values.user.statuses_count").alias("statuses_count"),col("values.user.favourites_count").alias("favourites_count"),col("values.user.profile_image_url").alias("profile_image_url"),col("values.user.friends_count").alias("friends_count"),col("values.user.id").alias("id_usr"),col("values.id").alias("id"),col("values.user.location").alias("location"),col("values.text").alias("text"),col("values.source").alias("source"),col("values.in_reply_to_status_id").alias("in_reply_to_status_id"),col("values.in_reply_to_status_id_str").alias("in_reply_to_status_id_str"),col("values.in_reply_to_user_id").alias("in_reply_to_user_id"),col("values.in_reply_to_user_id_str").alias("in_reply_to_user_id_str"),col("values.in_reply_to_screen_name").alias("in_reply_to_screen_name"),col("values.geo").alias("geo"),col("values.coordinates").alias("coordinates"),col("values.place").alias("place"),col("values.favorite_count").alias("favorite_count"),col("values.lang").alias("lang"),col("values.entities.hashtags.text").alias("hashtags"))
 
-df_dedup = flattenedJsonDf.dropDuplicates("id")
-
-
-query = df_dedup.writeStream.format("delta").outputMode("append").option("checkpointLocation", checkpoint_dir).start(delta_loc)
+query = flattenedJsonDf.writeStream.format("delta").outputMode("append").option("checkpointLocation", checkpoint_dir).start(delta_loc)
 
 query.awaitTermination()
